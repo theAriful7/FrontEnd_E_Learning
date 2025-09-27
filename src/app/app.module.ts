82a1194401406e8c278afea3d5d11df5b49752d4
Mohammad Ariful <arifulmhm@gmail.com>
2025-09-27 07:54:58 +0600

    Add all missing Angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { StudentNavbarComponent } from './students/student-navbar/student-navbar.component';
+import { StudentListComponent } from './students/student-list/student-list.component';
+import { StudentFormComponent } from './students/student-form/student-form.component';
+import { StudentDashboardComponent } from './students/student-dashboard/student-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    StudentNavbarComponent,
+    StudentListComponent,
+    StudentFormComponent,
+    StudentDashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/students/student-navbar/student-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-navbar',
+  templateUrl: './student-navbar.component.html',
+  styleUrls: ['./student-navbar.component.css']
+})
+export class StudentNavbarComponent {
+
+}
